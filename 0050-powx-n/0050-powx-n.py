2a6cf8f5905d1dded1fa29a5353d7d09590c649a
Roneel-govekar <126224148+Roneel-govekar@users.noreply.github.com>
2023-02-24 00:35:48 -0500

    Time: 45 ms (5.57%), Space: 25.3 MB (7.20%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+
+class Solution():
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return np.power(x,n)
+            
